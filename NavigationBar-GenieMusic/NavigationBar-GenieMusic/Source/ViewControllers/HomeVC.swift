c430de1a1c050032ef0bb56663ea1c1fa4700ed4
yangsubinn <cindy010670@gmail.com>
2022-02-14 21:52:31 +0900

    [Chore] NavigationBar-GenieMusic 폴더링

---
+++ NavigationBar-GenieMusic/NavigationBar-GenieMusic/Source/ViewControllers/HomeVC.swift
@@ -0,0 +1,29 @@
+//
+//  HomeVC.swift
+//  NavigationBar-GenieMusic
+//
+//  Created by 양수빈 on 2022/02/14.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
